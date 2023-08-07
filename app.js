3f573297249c059d978e6a33c5243819919c7b16
F3ll4s <141587053+F3ll4s@users.noreply.github.com>
2023-08-07 20:48:48 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -11,11 +11,33 @@ function typewritter(element, text, index,speed) {
 const SIRAPOP = "SIRAPOP_";
 typeWriter(sir, SIRAPOP, 0, 50);
 
-
-
 const sections = document.querySelectorAll('section');
-navLink = document.querySelectorAll('nav a');
+const navLinks = document.querySelectorAll('nav a');
 
+function setActiveLink() {
+    const scrollY = window.scrollY;
+    sections.forEach((section, index) => {
+      const sectionTop = section.offsetTop - 100;
+      const sectionBottom = sectionTop + section.offsetHeight;
+      if (scrollY >= sectionTop && scrollY < sectionBottom) {
+        navLinks.forEach((link) => link.classList.remove('active'));
+        navLinks.forEach((link) => link.classList.add('inactive'));
+        navLinks[index].classList.add('active');
+        navLinks[index].classList.remove('inactive');
+      }
+    });
+  }
+  
+  function stickyNav() {
+    const nav = document.querySelector('nav');
+    const headerHeight = document.querySelector('header').offsetHeight;
+    if (window.scrollY >= headerHeight) {
+      nav.classList.add('sticky');
+    } else {
+      nav.classList.remove('sticky');
+    }
+  }
+  
 function smoothScroll(event) {
     event.preventDefault();
     const targetID = event.target.getAttribute('href');
