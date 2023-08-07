6448979be885685251151265c795e15430d65b8e
F3ll4s <141587053+F3ll4s@users.noreply.github.com>
2023-08-07 22:27:32 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,61 +1,51 @@
-// Get the button and content elements
 const sir = document.getElementById("sir");
 
-// Function to simulate typing effect
-function typeWriter(element, text, index, speed) {
-  if (index < text.length) {
-      element.innerHTML += text.charAt(index);
-      index++;
-      setTimeout(() => typeWriter(element, text, index, speed), speed);
-  }
+function typewritter(element, text, index,speed) {
+    if (index < text.length) {
+        element.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(() => typewritter(element, text,index, speed), speed);
+    }
 }
 
-// Typing effect on rackroll_ text
 const SIRAPOP = "Sirapop_";
-typeWriter(sir, SIRAPOP, 0, 50);
+typewritter(sir, SIRAPOP, 0, 50);
 
 const sections = document.querySelectorAll('section');
-const navLink = document.querySelectorAll('nav a');
+const navLinks = document.querySelectorAll('nav a');
 
 function setActiveLink() {
-  const scrollY = window.scrollY;
-  sections.forEach((section, index) => {
-    const sectionTop = section.offsetTop - 100;
-    const sectionBottom = sectionTop + section.offsetHeight;
-    if (scrollY >= sectionTop && scrollY < sectionBottom) {
-      navLink.forEach((link) => link.classList.remove('active'));
-      navLink.forEach((link) => link.classList.add('inactive'));
-      navLink[index].classList.add('active');
-      navLink[index].classList.remove('inactive');
-    }
-  });
-}
-
-function stickyNav() {
-  const nav = document.querySelector('nav');
-  const headerHeight = document.querySelector('header').offsetHeight;
-  if (window.scrollY >= headerHeight) {
-    nav.classList.add('sticky');
-  } else {
-    nav.classList.remove('sticky');
+    const scrollY = window.scrollY;
+    sections.forEach((section, index) => {
+      const sectionTop = section.offsetTop - 100;
+      const sectionBottom = sectionTop + section.offsetHeight;
+      if (scrollY >= sectionTop && scrollY < sectionBottom) {
+        navLinks.forEach((link) => link.classList.remove('active'));
+        navLinks.forEach((link) => link.classList.add('inactive'));
+        navLinks[index].classList.add('active');
+        navLinks[index].classList.remove('inactive');
+      }
+    });
   }
-}
-
-//Smooth Scroll
+  
+  function stickyNav() {
+    const nav = document.querySelector('nav');
+    const headerHeight = document.querySelector('header').offsetHeight;
+    if (window.scrollY >= headerHeight) {
+      nav.classList.add('sticky');
+    } else {
+      nav.classList.remove('sticky');
+    }
+  }
 
 function smoothScroll(event) {
-  event.preventDefault();
-  const targetId = event.target.getAttribute('href');
-  const targetSection = document.querySelector(targetId);
-  targetSection.scrollIntoView({ behavior: 'smooth' });
+    event.preventDefault();
+    const targetID = event.target.getAttribute('href');
+    const targetSelection = document.querySelectorAll(targetID);
+    targetSelection.scrollIntoView({behavior : 'smooth'});
 }
 
-navLink.forEach(link => {
-  link.addEventListener('click', smoothScroll);
-});
+navLinks.arrary.forEach(link => {
+    link.addEventListener('click'. smoothScroll);
 
-
-window.addEventListener('scroll', () => {
-  setActiveLink();
-  stickyNav();
 });
