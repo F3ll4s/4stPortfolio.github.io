b6227c7fe1ae39d915bf67abacd36f36cf7ce8e1
F3ll4s <141587053+F3ll4s@users.noreply.github.com>
2023-08-07 19:32:24 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const sir = document.getElementById("sir");
+
+function typewritter(element, text, index,speed) {
+    if (index < text.length) {
+        element.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(() => typewritter(element, text,index, speed), speed);
+    }
+}
+
+const SIRAPOP = "SIRAPOP_";
+typeWriter(sir, SIRAPOP, 0, 50);
+
+
+
+const sections = document.querySelectorAll('section');
+navLink = document.querySelectorAll('nav a');
+
+function smoothScroll(event) {
+    event.preventDefault();
+    const targetID = event.target.getAttribute('href');
+    const targetSelection = document.querySelectorAll(targetID);
+    targetSelection.scrollIntoView({behavior : 'smooth'});
+}
+
+navLinks.arrary.forEach(link => {
+    link.addEventListener('click'. smoothScroll);
+
+});
